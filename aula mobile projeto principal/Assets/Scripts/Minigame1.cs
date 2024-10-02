e716d1bd2cc9f5cfd35ab0ecaa72b57f2ac21b91
TangyPlayer100K <156476806+TangyPlayer100K@users.noreply.github.com>
2024-10-02 20:38:42 -0300

    fume me

---
+++ aula mobile projeto principal/Assets/Scripts/Minigame1.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Minigame1 : MonoBehaviour
+{
+    [SerializeField] private float maxBar = 100f;
+    [SerializeField] private float currentBar = 0;
+    [SerializeField] private Slider barra = null;
+    [SerializeField] private float score = 0;
+    [SerializeField] private float time;
+    [SerializeField] private GameObject gameOverPanel = null;
+    [SerializeField] private TextMeshProUGUI scoreText = null;
+
+    void Start()
+    {
+        if (gameOverPanel == null)
+        {
+            print("n colocou o panel");
+
+        }
+        if (barra == null)
+        {
+            print("n colocou a barra");
+
+        }
+        if (scoreText == null)
+        {
+            print("n colocou o text");
+
+        }
+        gameOverPanel.SetActive(false);
+        barra.maxValue = maxBar;
+    }
+    
+    void Update()
+    {
+        if (currentBar > maxBar)
+        {
+            currentBar = maxBar;
+        }
+        barra.value = currentBar;
+
+        if (currentBar > 0)
+        {
+            currentBar -= 10f * Time.deltaTime;
+        }
+        if (time > 0)
+        {
+            time -= 1f * Time.deltaTime;
+        }
+        if (time <= 0)
+        {
+            GameOver();
+        }
+
+        if (barra.value > 25 % maxBar && barra.value < 50 % maxBar)
+        {
+            score += 25f * Time.deltaTime;
+        }
+        if (barra.value > 50 % maxBar && barra.value < 75 % maxBar)
+        {
+            score += 50f * Time.deltaTime;
+        }
+        if (barra.value > 75 % maxBar && barra.value < 95 % maxBar)
+        {
+            score += 75f * Time.deltaTime;
+        }
+        if (barra.value > 95 % maxBar)
+        {
+            score += 100f * Time.deltaTime;
+        }
+    }
+
+    public void ActionButton()
+    {
+        currentBar += 10f;
+    }
+
+    public void GameOver()
+    {
+        scoreText.text = "score maximo: " + score;
+        gameOverPanel.SetActive(true);
+        PlayerPrefs.SetFloat("maxScoreMinigame1", score);
+    }
+}
